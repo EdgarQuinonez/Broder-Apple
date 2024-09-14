fb7a442c96a85f79615012f8235c61e856573ad9
EdgarGlowbo <efqr811@gmail.com>
2024-09-13 19:44:30 -0600

    input number default styles overwritten

--- src/app/pages/operations/income/income.component.ts
+++ src/app/pages/operations/income/income.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { LucideAngularModule, XIcon } from 'lucide-angular';
 
 @Component({
   selector: 'app-income',
   standalone: true,
-  imports: [],
+  imports: [LucideAngularModule, RouterLink, RouterLinkActive],
   templateUrl: './income.component.html',
-  styleUrl: './income.component.scss'
+  styleUrl: './income.component.scss',
 })
 export class IncomeComponent {
-
+  XIcon = XIcon;
 }
