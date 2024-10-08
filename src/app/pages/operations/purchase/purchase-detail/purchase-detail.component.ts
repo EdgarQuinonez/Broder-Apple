eab24d9a35540142bfd148c1ec34fed37576a509
EdgarGlowbo <efqr811@gmail.com>
2024-10-07 21:50:23 -0600

    fake actorList for dynamic form controls. Validators not yet implemented.

--- src/app/pages/operations/purchase/purchase-detail/purchase-detail.component.ts
+++ src/app/pages/operations/purchase/purchase-detail/purchase-detail.component.ts
@@ -1,4 +1,4 @@
-import { NgIf } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import {
   Router,
@@ -17,7 +17,15 @@ import {
 import { Observable, of } from 'rxjs';
 import { Location } from '@angular/common'; // Import Location service
 import { InputRadioComponent } from '@shared/input-radio/input-radio.component';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  Validators,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  AbstractControl,
+} from '@angular/forms';
 import {
   Chart,
   ChartType,
@@ -32,6 +40,7 @@ import { BaseChartDirective } from 'ng2-charts';
   standalone: true,
   imports: [
     NgIf,
+    NgFor,
     LucideAngularModule,
     RouterLink,
     RouterLinkActive,
@@ -52,34 +61,44 @@ export class PurchaseDetailComponent {
   selectedProduct: any;
   totalCost: number = 0;
 
-  // TODO: Fetch actors from the API.
+  // TODO: Fetch actors from API
+  actorList = [
+    {
+      id: 1,
+      name: 'Investor 1',
+      // TODO: Perform calculations to determine the percentage based on the quantity.
+      quantity: 50,
+      percentage: 50,
+    },
+    {
+      id: 2,
+      name: 'Investor 2',
+      quantity: 50,
+      percentage: 50,
+    },
+  ];
+
   // ------------------ Doughnut Chart Configuration ------------------
 
   public doughnutChartLabels: string[] = ['Investor 1', 'Investor 2'];
-
+  // TODO: Have a switch case to determine the colors based on the number of actors.
   // TODO: Fix colors and change stroke width to be thinner. Fix dimensions and make it responsive.
   public doughnutChartDatasets: ChartConfiguration<'doughnut'>['data']['datasets'] =
     [
       {
-        // TODO: Data coming from actor's input fields
+        // TODO: Data comes from actors quantity array.
         data: [50, 50],
         backgroundColor: ['#42A5F5', '#66BB6A'],
         hoverBackgroundColor: ['#64B5F6', '#81C784'],
       },
     ];
 
-  //
+  // TODO: Remove labels. Only the doughnut chart should be displayed.
   public doughnutChartOptions: ChartOptions<'doughnut'> = {
     responsive: false,
     plugins: {
       tooltip: {
         enabled: false,
-        // TODO: Remove labels. Only the doughnut chart should be displayed.
-        callbacks: {
-          label: function (tooltipItem) {
-            return tooltipItem.label;
-          },
-        },
       },
       legend: {
         display: false,
@@ -94,14 +113,41 @@ export class PurchaseDetailComponent {
   isBank = this.paymentMethod === 'bank';
 
   transactionForm = new FormGroup({
-    type: new FormControl('income'),
-    amount: new FormControl(0),
-    description: new FormControl(''),
     paymentMethod: new FormControl('cash'),
+    actors: new FormArray(
+      this.actorList.map(
+        () =>
+          new FormGroup({
+            quantity: new FormControl(0),
+            percentage: new FormControl(0),
+          })
+      )
+    ),
   });
 
+  constructor(private route: ActivatedRoute, private _location: Location) {}
+
+  get actors(): FormArray {
+    return this.transactionForm.get('actors') as FormArray;
+  }
+
+  // Remove actor by index
+  removeActor(index: number): void {
+    this.actors.removeAt(index);
+  }
+
+  // Safely get the quantity FormControl for each actor
+  getQuantityControl(actor: AbstractControl): FormControl {
+    return actor.get('quantity') as FormControl;
+  }
+
+  // Safely get the percentage FormControl for each actor
+  getPercentageControl(actor: AbstractControl): FormControl {
+    return actor.get('percentage') as FormControl;
+  }
+
   // TODO: Mocked product data (replace this with service calls if you have an API)
-  // TODO: Add product types
+  // TODO: Add product types interface
   allProducts = [
     {
       id: 1,
@@ -157,8 +203,6 @@ export class PurchaseDetailComponent {
     },
   ];
 
-  constructor(private route: ActivatedRoute, private _location: Location) {}
-
   ngOnInit() {
     // Fetch the product ID from the route
     const productId = Number(this.route.snapshot.paramMap.get('id'));
@@ -172,13 +216,22 @@ export class PurchaseDetailComponent {
       this.selectedProduct.price + this.selectedProduct.shippingCosts;
   }
 
-  // Method to go back to the previous page
   goBack() {
     this._location.back();
   }
 
   // TODO: Perform double entry accounting movements for the purchase and add the purchased item to inventory.
   handleSubmit() {
-    console.log('Form submitted');
+    const bodyData = {
+      product: this.selectedProduct,
+      actors: this.transactionForm.value.actors,
+      paymentMethod: this.transactionForm.value.paymentMethod,
+      totalCost: this.totalCost,
+    };
+
+    console.log(bodyData);
+
+    // TODO: Once transaction is successful, it should create a row for Inventory for new product.
+    // TODO: Debit the Inventory account and credit the Cash/Bank account for each actor.
   }
 }
