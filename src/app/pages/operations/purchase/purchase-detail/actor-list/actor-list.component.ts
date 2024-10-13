e27e5214f89d546226d34f309f000998fbb907ea
EdgarGlowbo <efqr811@gmail.com>
2024-10-12 18:28:02 -0600

    Type casted for actorFormArray and actorControls

--- src/app/pages/operations/purchase/purchase-detail/actor-list/actor-list.component.ts
+++ src/app/pages/operations/purchase/purchase-detail/actor-list/actor-list.component.ts
@@ -1,45 +1,119 @@
 import { Component, Input } from '@angular/core';
 import { ActorBase } from '../purchase-detail.component';
-import { FormArray, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormArray,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import {
+  LockIcon,
+  LockOpenIcon,
+  LucideAngularModule,
+  TrashIcon,
+} from 'lucide-angular';
 
 @Component({
   selector: 'app-actor-list',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, LucideAngularModule],
   templateUrl: './actor-list.component.html',
   styleUrl: './actor-list.component.scss',
 })
 export class ActorListComponent {
   @Input() actors!: ActorBase[];
-  @Input() actorControls!: any;
+  @Input() actorsFormArray!: FormArray;
   @Input() totalCost!: number;
 
-  onQuantityChange(newQuantity: number, actorIndex: number): void {
-    const excludeActorID = this.actors[actorIndex].name; // TODO: When api is ready, use actor ID
-    this.distributeContributions(newQuantity, excludeActorID);
+  LockIcon = LockIcon;
+  LockOpenIcon = LockOpenIcon;
+  TrashIcon = TrashIcon;
+
+  excludedActorIDs: string[] = [];
+  disabledActorsInputs: boolean[] = [];
+
+  getActorControls(actorIndex: number): FormGroup | null {
+    return (this.actorsFormArray.at(actorIndex) as FormGroup) || null;
   }
 
-  // Distributes contributions among other actors, keeping the total investment constant
-  distributeContributions(newQuantity: number, excludeActorID: string): void {
+  getActorQuantityControl(actorIndex: number): FormControl {
+    const actorControl = this.getActorControls(actorIndex);
+    return actorControl?.get('quantity') as FormControl;
+  }
+
+  getActorPercentageControl(actorIndex: number): FormControl {
+    const actorControl = this.getActorControls(actorIndex);
+    return actorControl?.get('percentage') as FormControl;
+  }
+
+  onQuantityChange(newQuantity: number, actorIndex: number): void {
+    const actorControl = this.getActorControls(actorIndex);
+    if (!actorControl) return;
+
+    const excludeActorID = this.actors[actorIndex]?.name; // Ensure actor exists
+    if (excludeActorID) {
+      this.updateExcludedActors(excludeActorID);
+      this.distributeContributions(newQuantity, this.excludedActorIDs);
+    }
+  }
+
+  isActorExcluded(actorName: string): boolean {
+    return this.excludedActorIDs.includes(actorName);
+  }
+
+  toggleActorLock(actorName: string): void {
+    if (this.isActorExcluded(actorName)) {
+      this.excludedActorIDs = this.excludedActorIDs.filter(
+        (id) => id !== actorName
+      );
+    } else {
+      this.excludedActorIDs.push(actorName);
+    }
+    this.updateActorInputsState();
+  }
+
+  updateActorInputsState(): void {
+    this.actors.forEach((actor, index) => {
+      const isDisabled = this.isActorExcluded(actor.name);
+      const actorControl = this.actorsFormArray.at(index) as FormGroup;
+      if (isDisabled) {
+        actorControl.get('quantity')?.disable();
+      } else {
+        actorControl.get('quantity')?.enable();
+      }
+    });
+  }
+
+  updateExcludedActors(newExcludedActorID: string): void {
+    const newExcludedActors = [...this.excludedActorIDs, newExcludedActorID];
+    this.excludedActorIDs = newExcludedActors;
+  }
+
+  removeActor(actorIndex: number): void {
+    this.actors.splice(actorIndex, 1);
+    this.actorsFormArray.removeAt(actorIndex);
+  }
+
+  distributeContributions(
+    newQuantity: number,
+    excludedActorIDs: string[]
+  ): void {
     const remainder = this.totalCost - newQuantity;
 
-    // Find the total quantity of all other actors, handling null/NaN cases
     const totalOtherQuantities = this.actors
-      .filter((actor) => actor.name !== excludeActorID)
+      .filter((actor) => !excludedActorIDs.includes(actor.name))
       .reduce((sum, actor, index) => {
         const currentQuantity = this.sanitizeValue(
-          this.actorControls.at(index).get('quantity')?.value
+          this.actorsFormArray.at(index).get('quantity')?.value
         );
         return sum + currentQuantity;
       }, 0);
 
-    // Avoid division by zero by handling case when totalOtherQuantities is 0
     if (totalOtherQuantities === 0) return;
 
-    // Update the quantities and percentages of the other actors
     this.actors.forEach((actor, index) => {
-      const actorControl = this.actorControls.at(index) as FormGroup;
-      if (actor.name !== excludeActorID) {
+      const actorControl = this.actorsFormArray.at(index) as FormGroup;
+      if (!excludedActorIDs.includes(actor.name)) {
         const currentQuantity = this.sanitizeValue(
           actorControl.get('quantity')?.value
         );
@@ -50,7 +124,7 @@ export class ActorListComponent {
           percentage: (newActorQuantity / this.totalCost) * 100 || 0,
         });
       } else {
-        // Update the percentage for the actor that was changed
+        // Set only the percentage for the excluded actors
         actorControl.patchValue({
           percentage: (newQuantity / this.totalCost) * 100 || 0,
         });
@@ -58,7 +132,6 @@ export class ActorListComponent {
     });
   }
 
-  // Helper method to sanitize values (converting null, undefined, or NaN to 0)
   private sanitizeValue(value: any): number {
     return isNaN(value) || value === null || value === undefined ? 0 : value;
   }
